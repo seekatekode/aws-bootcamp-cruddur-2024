1601fa153d9fb43ce6682d7373f94283eb4b9910
seekatekode <katelyn.nettles@gmail.com>
2023-06-06 22:28:24 +0000

    notif activity.py update

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,47 +1,26 @@
 from datetime import datetime, timedelta, timezone
-
-
-
 class NotificationsActivities:
   def run():
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Whitney Houston',
-      'message': 'I wanna dance with somebody!',
+      'message': 'I am every woman!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
       'replies_count': 1,
       'reposts_count': 0,
-      'replies': 3 [{
+      'replies': [{
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
-        'handle':  'Usher',
-        'message': 'These are my confessions!',
+        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'Bobby Brown',
+        'message': 'Parachute pants will always be in style!',
         'likes_count': 0,
         'replies_count': 0,
         'reposts_count': 0,
-        'created_at': (now - timedelta(days=2)).isoformat(),
-        'expires_at': (now + timedelta(days=5)).isoformat(),
+        'created_at': (now - timedelta(days=2)).isoformat()
       }],
-    },
-    {
-      'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-      'handle':  'Madea',
-      'message': 'Oh, heller!',
-      'created_at': (now - timedelta(days=7)).isoformat(),
-      'expires_at': (now + timedelta(days=9)).isoformat(),
-      'likes': 0,
-      'replies': 3
-    },
-    {
-      'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-      'handle':  'Moneybagg Yo',
-      'message': 'You doin too much!',
-      'created_at': (now - timedelta(hours=1)).isoformat(),
-      'expires_at': (now + timedelta(hours=12)).isoformat(),
-      'likes': 0,
-      'replies': 23
     }
     ]
     return results
