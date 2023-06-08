7a904cff3552f580843cdcfad25b98fa2113233b
seekatekode <katelyn.nettles@gmail.com>
2023-06-08 18:38:00 +0000

    seed file update

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,6 +3,7 @@ INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Marie', 'katelyn.nettles@gmail.com', 'Marie', 'f333ba2f-e584-467e-9535-b828c4ba13a8'),
   ('kate nettles', 'katelyn_nett06@yahoo.com', 'katenettles', '04fae33c-a228-41d4-9a21-f3a76b6f0ce3'),
+  ('Granite Universe','colorisinthestars@icloud.com','Granite','MOCK'),
   ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
